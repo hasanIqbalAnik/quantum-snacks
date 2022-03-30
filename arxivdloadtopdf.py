004de946a4fb100d555a3063ceae69c65fd709b9
hasaniqbalanik <hasan.iqbal.anik@gmail.com>
2022-03-30 00:31:55 -0400

    fix

--- arxivdloadtopdf.py
+++ arxivdloadtopdf.py
@@ -3,40 +3,46 @@ from fpdf import FPDF
 from pylatex import Document, Section, Subsection, Command
 from pylatex.utils import italic, NoEscape
 
+def fill_document(query, doc):
+	articles = arxivpy.query(search_query=query, start_index=0,max_index=12,results_per_iteration=100,wait_time=5.0, sort_by='lastUpdatedDate')
+	for article in articles:
+		try:
+			with doc.create(Subsection(NoEscape(article['title']))):
+					doc.append(NoEscape(r'\paragraph{}'))
+					doc.append(article['authors'])
+					doc.append('\n')
+					doc.append(article['update_date'])
+					doc.append('\n')
+					doc.append(NoEscape(article['abstract']))
+					
+		except Exception as e:
+			print(e)
+			pass
+
+query1='all:Quantum+AND+all:Cryptography+AND+abs:%22quantum+key+distribution%22'
+query2 = 'abs:%22quantum+information+theory%22'
+query3 = 'abs:%22entropic+uncertainty+relation%22'
+
+geometry_options = {"margin": "0.2in"}
+doc = Document(geometry_options=geometry_options)
+
+doc.change_document_style("empty")
 
 
-# articles = arxivpy.query(search_query='',
-#                          start_index=0, max_index=20, results_per_iteration=100,
-#                          wait_time=5.0, sort_by='lastUpdatedDate')    
+    # geometry_options = {
+    #     "landscape": True,
+    #     "margin": "1.5in",
+    #     "headheight": "20pt",
+    #     "headsep": "10pt",
+    #     "includeheadfoot": True
+    # }
+# doc.preamble.append(Command('title', 'Daily Quantum Cryptography Snacks'))
+# doc.preamble.append(Command('author', 'Everyone'))
+# doc.preamble.append(Command('date', NoEscape(r'\today')))
+# doc.append(NoEscape(r'\maketitle')))
 
-query='all:Quantum+AND+all:Cryptography+AND+all:key+AND+distribution+AND+cat:quant-ph'
-print(query)
-articles = arxivpy.query(search_query=query, start_index=0,max_index=10,results_per_iteration=100,wait_time=5.0, sort_by='lastUpdatedDate')
-
-# articles = arxivpy.query(search_query=['quant-ph'],
-#                          start_index=0, max_index=10, results_per_iteration=100,
-#                          wait_time=5.0, sort_by='lastUpdatedDate') # grab 200 articles
-
-
-doc = Document()
-doc.preamble.append(Command('title', 'Daily Quantum Cryptography Snacks'))
-doc.preamble.append(Command('author', 'Everyone'))
-doc.preamble.append(Command('date', NoEscape(r'\today')))
-doc.append(NoEscape(r'\maketitle'))
-
-# fill_document(doc)
-# doc.generate_pdf('basic_maketitle', clean_tex=False)
-# Add stuff to the document
-for article in articles:
-	try:
-		with doc.create(Subsection(NoEscape(article['title']))):
-				doc.append(NoEscape(r'\paragraph{}'))
-				doc.append(article['authors'])
-				doc.append(NoEscape(article['abstract']))
-	except Exception as e:
-		print(e)
-		pass
-
+fill_document(query1, doc)
+fill_document(query2, doc)
+fill_document(query3, doc)
 
 doc.generate_pdf('snacks', clean_tex=False)
-tex = doc.dumps()  # The document as string in LaTeX syntax
