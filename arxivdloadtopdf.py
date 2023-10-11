2bd6b421933502627921265acdf96bf81b4d9b09
Hasan Iqbal <hasan.iqbal.anik@gmail.com>
2023-10-11 18:32:13 -0400

    fix

--- arxivdloadtopdf.py
+++ arxivdloadtopdf.py
@@ -1,26 +1,29 @@
-import arxivpy
+import arxiv
 from pylatex import Document, Subsection
 from pylatex.utils import  NoEscape
 
 def fill_document(query, doc):
-	articles = arxivpy.query(search_query=query, start_index=0,max_index=12,results_per_iteration=100,wait_time=5.0, sort_by='lastUpdatedDate')
-	for article in articles:
+	search = arxiv.Search(
+	  query = 'all: quantum AND key AND distribution',
+	  max_results = 15,
+	  sort_by = arxiv.SortCriterion.SubmittedDate
+	)
+	for article in search.results():
 		try:
-			with doc.create(Subsection(NoEscape(article['title']))):
+			with doc.create(Subsection(NoEscape(article.title))):
 				doc.append(NoEscape(r'\paragraph{}'))
-				doc.append(article['authors'])
+				doc.append(', '.join([item.name for item in article.authors]))
 				doc.append('\n')
-				doc.append(article['update_date'])
+				doc.append(article.updated)
 				doc.append('\n')
-				doc.append(NoEscape(article['abstract']))
+				doc.append(NoEscape(article.summary))
 					
 		except Exception as e:
 			print(e)
 			pass
 
-query1='all:Quantum+AND+all:Cryptography+AND+abs:%22quantum+key+distribution%22'
-query2 = 'abs:%22quantum+information+theory%22'
-query3 = 'abs:%22entropic+uncertainty+relation%22'
+query1='all:quantum AND key AND distribution AND information AND theory AND mutual AND information'
+
 
 geometry_options = {"margin": "0.4in"}
 doc = Document(geometry_options=geometry_options)
@@ -33,8 +36,7 @@ doc.change_document_style("empty")
 #     "includeheadfoot": True
 # }
 
-fill_document(query1, doc)
-fill_document(query2, doc)
-fill_document(query3, doc)
+fill_document(query1, doc)#
+
 
 doc.generate_pdf('snacks', clean_tex=False)
